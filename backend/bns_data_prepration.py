a88133fda9e1258f695a601ef57b8e8725c0637e
Turbash Negi <negirawatdeepi@gmail.com>
2025-08-19 12:30:30 +0530

    Finally fixed that extra section somehow i don'e know how the regex has worked now it should not work but it does so i a not going to touch it i got the data that i wanted now

--- backend/bns_data_prepration.py
+++ backend/bns_data_prepration.py
@@ -1,7 +1,11 @@
 from PyPDF2 import PdfReader
 import re
+import csv
+import json
 
 pdf_path = "../data/bns.pdf"
+output_json = "bns_sections.json"
+output_csv = "bns_sections.csv"
 
 reader = PdfReader(pdf_path)
 
@@ -12,10 +16,29 @@ for page in reader.pages:
     if t:
         text += t + "\n"
 
-sections = re.split(r'(?m)^\d+\.(?=\D)', text)
+sections = re.split(r'(?m)^(\d+)\.(?:\(\d+\))?(?:\s(?=W))?', text)
+print(sections[:3])
+cleaned_sections = []
 
-cleaned_sections = [sec.strip() for sec in sections if sec.strip()]
+for i in range(1, len(sections), 2):
+    section_number = int(sections[i])
+    section_text = sections[i+1].strip()
+    section_text = re.sub(r'\s+', ' ', section_text)
 
-for i, section in enumerate(cleaned_sections):
-    print(f"\n--Section {i} --\n")
-    print(section)
+    cleaned_sections.append({
+        "section_number": section_number,
+        "text": section_text
+    })
+
+print("Total sections found:", len(cleaned_sections))
+
+with open(output_csv, 'w', encoding = "UTF-8", newline="") as f:
+    writer = csv.DictWriter(f, fieldnames=["section_number", "text"])
+    writer.writeheader()
+    for row in cleaned_sections:
+        writer.writerow(row)
+
+with open(output_json, 'w', encoding="UTF-8") as f:
+    json.dump(cleaned_sections, f, ensure_ascii=False, indent=2)
+
+print("Data saved to", output_json, "and", output_csv)
