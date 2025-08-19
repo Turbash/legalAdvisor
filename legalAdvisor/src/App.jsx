17681faeb6d114d807d8ff7c49ae6bdc162d21c7
Turbash Negi <negirawatdeepi@gmail.com>
2025-08-19 11:21:07 +0530

    Just adding empty frontend and a gitignore to avoid pushing the pdfds

---
+++ legalAdvisor/src/App.jsx
@@ -0,0 +1,10 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <p>Hello ye raha ek Indian law advisor</p>
+  )
+}
+
+export default App
