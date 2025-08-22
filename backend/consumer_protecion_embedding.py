86b7fd941ecff0210eb2c2fb5f289bc478158452
Turbash Negi <negirawatdeepi@gmail.com>
2025-08-22 14:37:35 +0530

    removed en i think.

---
+++ backend/consumer_protecion_embedding.py
@@ -0,0 +1,39 @@
+from sentence_transformers import SentenceTransformer
+import chromadb
+import json
+
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+client = chromadb.PersistentClient(path="chroma_db")
+
+collection = client.get_collection(name="legal_docs")
+
+all_docs, all_ids, all_metadatas = [], [], []
+
+with open("consumer_protection_sections.json", "r") as f:
+    data = json.load(f)
+    for item in data:
+        unique_id=f"{item['law_id']}_{item['section_number']}"
+        all_docs.append(item["text"])
+        all_ids.append(unique_id)
+        all_metadatas.append({"law": item["law_title"],"preamble": item["preamble"], "section_number": item["section_number"],"text": item["text"]})
+
+embeddings = model.encode(all_docs).tolist()
+
+collection.add(
+    documents=all_docs,
+    embeddings=embeddings,
+    ids=all_ids,
+    metadatas=all_metadatas
+)
+
+print(f"✅ Stored {len(all_docs)} law sections into Chroma from consumer_protection32_sections.json!")
+
+query = "What if I fail to comply with an order?"
+query_emb = model.encode([query]).tolist()
+
+results = collection.query(
+    query_embeddings=query_emb,
+    n_results=4
+)
+
+print(results)
